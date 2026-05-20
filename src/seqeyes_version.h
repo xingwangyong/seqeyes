52051b6f7d854efee2216ed1572582db9be8e923
Xingwang Yong <xingwangyong@zju.edu.cn>
2026-05-20 15:53:48 -0400

    chore: v0.0.8

--- src/seqeyes_version.h
+++ src/seqeyes_version.h
@@ -2,7 +2,7 @@
 
 // Manual application semantic version. Update as needed for releases.
 // Keep both a plain and a display form.
-#define SEQEYES_APP_VERSION_PLAIN "0.0.7"
+#define SEQEYES_APP_VERSION_PLAIN "0.0.8"
 #define SEQEYES_APP_VERSION "v" SEQEYES_APP_VERSION_PLAIN
 
 
